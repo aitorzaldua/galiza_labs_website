3748e3cab20ccfbc223f5289fe5d8211ce2520ff
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2023-01-13 14:58:10 +0100

    contact form, add background

--- pages/Contact.jsx
+++ pages/Contact.jsx
@@ -1,9 +1,16 @@
 export default function Contact() {
   return (
-    <div className='flex w-full justify-center items-center 2xl:pc-20 gradient-bg-transactions'>
-      <div className='flex flex-col md:p-12 py-12 px-4'>
-        Welcome to Contact page
+    <div className="flex flex-col w-full justify-center items-center gradient-bg-services">
+      {/* Tittle */}
+      <div className="flex mf:flex-row flex-col items-center justify-between py-12 px-4 md:p-20 sm:p-10">
+        <div className="flex-1 flex flex-col justify-start items-start">
+          <h1 className="text-white text-3xl sm:text-5xl py-2">Contact Us</h1>
+        </div>
       </div>
+
+      {/* Contact Area */}
+
+      {/* Image */}
     </div>
-  )
+  );
 }
--- pages/Services.jsx
+++ pages/Services.jsx
@@ -1,7 +1,7 @@
-import { SiWeb3Dotjs } from 'react-icons/si';
-import { BsShieldFillCheck } from 'react-icons/bs';
-import { TiPencil } from 'react-icons/ti';
-import { ImNewspaper } from 'react-icons/im';
+import { SiWeb3Dotjs } from "react-icons/si";
+import { BsShieldFillCheck } from "react-icons/bs";
+import { TiPencil } from "react-icons/ti";
+import { ImNewspaper } from "react-icons/im";
 
 const ServiceCard = ({ color, title, icon, subtitle }) => (
   <div className="flex flex-row  justify-start items-center white-glassmorphism p-3 m-2 cursor-pointer hover:shadow-xl hover:bg-[#383f51]">
@@ -28,77 +28,77 @@ export default function Services() {
         </div>
       </div>
 
-       <div className="flex-1 flex flex-row justify-start items-center w-[85rem]">
-         <ServiceCard
-           color='bg-[#8945f8]'
-           title="Web3 Projects"
-           icon={<SiWeb3Dotjs  fontSize={22} className="text-white"/>}
-           subtitle={
-             <a>
-               We help you identify opportunities to improve user experience,
-               supplier relationships or increase performance with the latest
-               cryptographic and economic innovations. <br />
-               New technological and economic infrastructures are constantly
-               being built and are accompanied by new trade-offs. Our research
-               team helps you identify the opportunities and risks inherent in
-               these new technologies as they relate to your specific project.
-             </a>
-           }
-         />
-         <ServiceCard
-           color='bg-[#65b800]'
-           title="Smart Contract Audits"
-           icon={<BsShieldFillCheck  fontSize={22} className="text-white"/>}
-           subtitle={
-             <a>
-                We help you identify opportunities to improve user experience,
-               supplier relationships or increase performance with the latest
-               cryptographic and economic innovations. <br />
-               New technological and economic infrastructures are constantly
-               being built and are accompanied by new trade-offs. Our research
-               team helps you identify the opportunities and risks inherent in
-               these new technologies as they relate to your specific project.
-             </a>
-           }
-         />
-       </div>
+      <div className="flex-1 flex flex-row justify-start items-center w-[85rem]">
+        <ServiceCard
+          color="bg-[#8945f8]"
+          title="Web3 Projects"
+          icon={<SiWeb3Dotjs fontSize={22} className="text-white" />}
+          subtitle={
+            <a>
+              We help you identify opportunities to improve user experience,
+              supplier relationships or increase performance with the latest
+              cryptographic and economic innovations. <br />
+              New technological and economic infrastructures are constantly
+              being built and are accompanied by new trade-offs. Our research
+              team helps you identify the opportunities and risks inherent in
+              these new technologies as they relate to your specific project.
+            </a>
+          }
+        />
+        <ServiceCard
+          color="bg-[#65b800]"
+          title="Smart Contract Audits"
+          icon={<BsShieldFillCheck fontSize={22} className="text-white" />}
+          subtitle={
+            <a>
+              We help you identify opportunities to improve user experience,
+              supplier relationships or increase performance with the latest
+              cryptographic and economic innovations. <br />
+              New technological and economic infrastructures are constantly
+              being built and are accompanied by new trade-offs. Our research
+              team helps you identify the opportunities and risks inherent in
+              these new technologies as they relate to your specific project.
+            </a>
+          }
+        />
+      </div>
 
-
-       <div className="flex-1 flex flex-row justify-start items-center w-[85rem]">
-         <ServiceCard
-           color='bg-[#25bdf8]'
-           title="Blockchain University"
-           icon={<TiPencil  fontSize={22} className="text-white"/>}
-           subtitle={
-             <a>
-               We help you identify opportunities to improve user experience,
-               supplier relationships or increase performance with the latest
-               cryptographic and economic innovations. <br />
-               New technological and economic infrastructures are constantly
-               being built and are accompanied by new trade-offs. Our research
-               team helps you identify the opportunities and risks inherent in
-               these new technologies as they relate to your specific project.
-             </a>
-           }
-         />
-         <ServiceCard
-           color='bg-[#f54952]'
-           title="News and Ecosystem Information"
-           icon={<ImNewspaper  fontSize={22} className="text-white"/>}
-           subtitle={
-             <a>
-                We help you identify opportunities to improve user experience,
-               supplier relationships or increase performance with the latest
-               cryptographic and economic innovations. <br />
-               New technological and economic infrastructures are constantly
-               being built and are accompanied by new trade-offs. Our research
-               team helps you identify the opportunities and risks inherent in
-               these new technologies as they relate to your specific project.
-             </a>
-           }
-         />
-       </div>
-
+      <div className="flex-1 flex flex-row justify-start items-center w-[85rem]">
+        <ServiceCard
+          color="bg-[#25bdf8]"
+          title="Blockchain University"
+          icon={<TiPencil fontSize={22} className="text-white" />}
+          subtitle={
+            <a>
+              We help you identify opportunities to improve user experience,
+              supplier relationships or increase performance with the latest
+              cryptographic and economic innovations. <br />
+              New technological and economic infrastructures are constantly
+              being built and are accompanied by new trade-offs. Our research
+              team helps you identify the opportunities and risks inherent in
+              these new technologies as they relate to your specific project.
+            </a>
+          }
+        />
+        <ServiceCard
+          color="bg-[#f54952]"
+          title="News and Ecosystem Information"
+          icon={<ImNewspaper fontSize={22} className="text-white" />}
+          subtitle={
+            <a>
+              We help you identify opportunities to improve user experience,
+              supplier relationships or increase performance with the latest
+              cryptographic and economic innovations. <br />
+              New technological and economic infrastructures are constantly
+              being built and are accompanied by new trade-offs. Our research
+              team helps you identify the opportunities and risks inherent in
+              these new technologies as they relate to your specific project.
+            </a>
+          }
+        />
+      </div>
     </div>
   );
 }
+
+
