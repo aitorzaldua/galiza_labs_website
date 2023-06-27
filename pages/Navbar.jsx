eedc017b2361d1d24de0f4f0c31cc53683e9cb60
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2023-06-27 20:43:06 +0200

    responsive design

--- pages/Navbar.jsx
+++ pages/Navbar.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { Link } from "react-scroll/modules";
 import logo from "../images/Logos/DDLogo6.png";
 import { useState } from "react";
-import { HiMenuAlt4 } from "react-icons/hi";
+import { HiMenu } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
 
 const NavbarItem = ({ title, classProps }) => {
@@ -20,7 +20,7 @@ export default function Navbar() {
       <div className="md:flex-[0.5] flex flex-row flex-initial justify-start items-center ">
         <Image src={logo} alt="logo" className="w-[12rem]" />
       </div>
-      <ul className="text-[#dce0e5] md:flex hidden list-none flex-row justify-between items-center flex-initial">
+      <ul className="text-[#8db9e2] md:flex hidden list-none flex-row justify-between items-center flex-initial">
         {/* This is all the new implementation for mobiles */}
         {["What we Do", "The Process", "Check Audits"].map((item, index) => (
           <NavbarItem key={item + index} title={item} />
@@ -32,7 +32,7 @@ export default function Navbar() {
           smooth={true}
           offset={-200}
           duration={500}
-          className="bg-[#8db9e2] py-2 px-7 mx-4  rounded-md cursor-pointer hover:bg-[#A0AECD] "
+          className="bg-[#1a4b61] py-2 px-7 mx-4  rounded-md cursor-pointer hover:bg-[#A0AECD] "
         >
           Request an audit
         </Link>
@@ -40,12 +40,12 @@ export default function Navbar() {
 
         <div className="flex relative">
           {toggleMenu
-            ? <AiOutlineClose fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(false)}/> 
-            : <HiMenuAlt4 fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(true)}/>}
+            ? <AiOutlineClose fontSize={28} className="text-[#8db9e2] md:hidden cursor-pointer" onClick={() => setToggleMenu(false)}/> 
+            : <HiMenu fontSize={28} className="text-[#8db9e2] md:hidden cursor-pointer" onClick={() => setToggleMenu(true)}/>}
             {toggleMenu && (
               <ul className="z-10 fixed top-0 -right-2 p-3 w-[70w] h-screen shadow-2xl 
               md:hidden list-none flex flex-col justify-start items-end rounded-md 
-              blue-glassmorphism text-white animate-slide-in">
+              blue-glassmorphism text-[#8db9e2] animate-slide-in">
                 <li className="text-xl w-full my-2">
                 <AiOutlineClose onClick={() => setToggleMenu(false)}/>
 
@@ -60,7 +60,7 @@ export default function Navbar() {
                 smooth={true}
                 offset={-200}
                 duration={500}
-                className="bg-[#8db9e2] py-2 px-7 mx-4  rounded-md cursor-pointer hover:bg-[#A0AECD] "
+                className="bg-[#1a4b61] py-2 px-7 mx-4  rounded-md cursor-pointer"
               >
                 Request an audit
               </Link>
