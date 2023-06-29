3f6c23733ed52a3c3d7c4db1e1207148f9c29227
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2023-06-29 19:34:51 +0200

    final touch

--- pages/Welcome.jsx
+++ pages/Welcome.jsx
@@ -4,15 +4,13 @@ import WelcomeImg from "../images/welcome/compas16.png";
 export default function Welcome() {
   return (
     <div className="flex w-full justify-center items-center 2xl:px-40 2xl:pb-">
-      
-
-      <div className="flex md:flex-row flex-col items-start justify-between  py-24 md:p-20 ">
-      <div className="flex md:hidden flex-initial  justify-center md:w-[32rem] w-[20rem] items-center md:pt-2 2xl:pt-0 md:pr-4 2xl:pr-0">
-        <Image src={WelcomeImg} alt="WelcomeImg" className="imgWelcome" />
-      </div>
+      <div className="flex md:flex-row flex-col items-start justify-between  md:py-24 py-12 md:p-20 ">
+        <div className="flex md:hidden flex-initial  justify-center md:w-[32rem] w-[20rem] items-center md:pt-2 2xl:pt-0 md:pr-4 2xl:pr-0">
+          <Image src={WelcomeImg} alt="WelcomeImg" className="imgWelcome" />
+        </div>
         {/* Left side */}
         <div className="flex flex-1 justify-start flex-col md:mr-10">
-          <p className="md:text-6xl text-4xl text-gradient md:pt-24 pt-10 px-16 font-light">
+          <p className="md:text-6xl text-2xl text-gradient md:pt-24 pt-10 px-16 font-light">
             Smart contract security services for EVM Blockchains
           </p>
 
